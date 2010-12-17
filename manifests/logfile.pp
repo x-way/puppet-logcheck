414d779ed6cbc7d00c8978257b395b59838bb876
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-12-17 09:25:52 +0100

    logcheck::logfile - added right management for final file. Logcheck user have to read it

--- manifests/logfile.pp
+++ manifests/logfile.pp
@@ -1,7 +1,12 @@
 define logcheck::logfile($ensure=present, $file) {
+  file {"/etc/logcheck/logcheck.logfiles":
+    owner   => root,
+    group   => logcheck,
+    mode    => 0640,
+  }
   common::concatfilepart {"$name":
-    ensure      => $ensure,
-    file        => "/etc/logcheck/logcheck.logfiles",
-    content     => "$file\n",
+    ensure  => $ensure,
+    file    => "/etc/logcheck/logcheck.logfiles",
+    content => "$file\n",
   }
 }
