17af7121f81d87bafd0f8c49ec38b41d8da138bd
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2012-08-23 14:58:24 +0200

    corrected logcheck to avoid duplicated definition for files

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,42 +1,30 @@
 class logcheck {
   # install package
-  package { "logcheck":
+  package {'logcheck':
     ensure => present,
   }
 
   # config
-  file {"/etc/logcheck/logcheck.conf":
-    ensure => present,
-    content => template("logcheck/logcheck.conf.erb"),
-    require => Package["logcheck"],
-    owner => root,
-    group => logcheck,
+  file {'/etc/logcheck/logcheck.conf':
+    ensure  => present,
+    content => template('logcheck/logcheck.conf.erb'),
+    require => Package['logcheck'],
+    owner   => root,
+    group   => logcheck,
   }
 
   include concat::setup
   concat {[
       '/etc/logcheck/logcheck.logfiles',
+      '/etc/logcheck/cracking.ignore.d/auto-puppet',
+      '/etc/logcheck/ignore.d.paranoid/auto-puppet',
       '/etc/logcheck/ignore.d.server/auto-puppet',
-      '/etc/logcheck/violations.ignore.d/auto-puppet',
-      '/etc/logcheck/cracking.ignore.d/auto-puppet'
+      '/etc/logcheck/ignore.d.workstation/auto-puppet',
+      '/etc/logcheck/violations.ignore.d/auto-puppet'
     ]:
-    owner => root,
-    group => root,
-    mode  => '0644',
-  }
-
-  # create auto-puppet files
-  file { [
-      "/etc/logcheck/cracking.ignore.d/auto-puppet",
-      "/etc/logcheck/ignore.d.paranoid/auto-puppet",
-      "/etc/logcheck/ignore.d.server/auto-puppet",
-      "/etc/logcheck/ignore.d.workstation/auto-puppet",
-      "/etc/logcheck/violations.ignore.d/auto-puppet"
-    ]:
-    ensure => present,
-    mode => 0644,
-    owner => root,
-    group => logcheck,
-    require => Package["logcheck"],
+    owner   => root,
+    group   => logcheck,
+    mode    => '0644',
+    require => Package['logcheck'],
   }
 }
