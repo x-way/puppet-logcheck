2c481abab5928b213e7444819e072e1235a514e5
Andreas Jaggi <andreas.jaggi@waterwave.ch>
2013-07-01 01:11:41 +0200

    Remove file declaration for concat-managed files

--- manifests/logfile.pp
+++ manifests/logfile.pp
@@ -1,9 +1,4 @@
 define logcheck::logfile($ensure=present, $file) {
-  file {"/etc/logcheck/logcheck.logfiles":
-    owner   => root,
-    group   => logcheck,
-    mode    => 0640,
-  }
   concat::fragment {"$name":
     ensure  => $ensure,
     target  => "/etc/logcheck/logcheck.logfiles",
